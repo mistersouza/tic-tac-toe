418b41f8f28d8267d9b0d7d22a9303ed56c115fc
mistersouza <>
2023-11-01 13:21:25 +0000

    Write `get_bot_next_move` and refactor `get_user_next_move`

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import random
 
-print("Hey there, welcome to my Tic Tac Toe showdown")
+
 
 board = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']]
 
@@ -20,18 +20,19 @@ def draw_board():
     print('+---+---+---+')
 
 
+
 def get_next_move():
     '''
     Prompt the user to input their next move for the Tic Tac Toe game.
     Returns:
-        - int. User's valid move (an integer between 1 and 9).
+    - int. User's valid move (an integer between 1 and 9).
     '''
     # Prompt user for input until a valid move is provided.
     while True:
         try: 
             next_move = int(input("What's your move, superstar? Pick a number from 1 to 9: "))
             if validate_move(next_move):
-                return next_move
+                return (next_move, 'X')
         # It handles non-numeric input
         except ValueError:
             print("Oops, that's not a valid move. Try again, champ!")
@@ -59,6 +60,25 @@ def validate_move(move):
         return False
     return True
 
+def get_bot_next_move():
+    '''
+    Generates random moves for the bot within the range of 1 to 9. It ensures
+    that the chosen position on the board is not already occupied by 'X' or 'O'.
+
+    Returns:
+    - Tuple[int, str]: The bot's valid move represented as (move, 'O').
+    '''
+    while True:
+        bot_next_move = random.randint(1, 9)
+        # Calculate the row and column indices based on the random move
+        row, col = divmod(bot_next_move - 1, 3)
+        # Check if the board position is not 'X' or 'O' (i.e., unoccupied)
+        if board[row][col] not in ['X', 'O']:
+            # Return the bot's valid move
+            return (bot_next_move, 'O')
+
+
+
 
 def update_board(board, move, mark):
     '''
@@ -74,9 +94,25 @@ def update_board(board, move, mark):
     '''
 
     # Calculates the corresponding row and
-    column indices for the move
     row, col = divmod(move - 1, 3)
     # Updates the board with the player's mark.
     board[row][col] = mark
 
 
+print("Hey there, welcome to my Tic Tac Toe showdown")
+
+# Draw the initial board
+draw_board()
+
+round = 1
+while round < 10: 
+    # Get user to input next move
+    (move, mark) = get_next_move() if round % 2 != 0 else get_bot_next_move()
+
+    # Update board
+    update_board(board, move, mark)
+
+    # Display the board
+    draw_board()
+
+    round += 1 
